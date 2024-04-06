a2c252c6ca9f7bd2f65b64c41bf99d14f9113e5a
Gaurav Garggit config --global user.name "Gaurav Garg <gauravgarg9136@gmail.com>
2024-04-06 10:46:07 +0530

    Line number 6 change

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -1,8 +1,8 @@
 package p1;
 
 public class Test1 {
-
 	public static void main(String[] args) {
-		System.out.print("In Test1 File");
+		System.out.println("In Test1 File");
+		System.out.println("Line number 6 change in Test1");
 	}
 }
